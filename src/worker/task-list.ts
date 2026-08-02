b0deed415f8a71085a551d91888033506e19a488
Mtzallqmy <mtzallqmy@gmail.com>
2026-08-02 18:11:14 +0300

    feat(worker): register sandbox tasks

--- src/worker/task-list.ts
+++ src/worker/task-list.ts
@@ -2,11 +2,23 @@ import type { TaskList } from "graphile-worker";
 import { agentRunResumeTask } from "@/worker/tasks/agent-run-resume";
 import { agentTeamRunTask } from "@/worker/tasks/agent-team-run";
 import { documentParseTask } from "@/worker/tasks/document-parse";
+import { sandboxCleanupTask } from "@/worker/tasks/sandbox-cleanup";
+import { sandboxCreateTask } from "@/worker/tasks/sandbox-create";
+import { sandboxExecuteTask } from "@/worker/tasks/sandbox-execute";
+import { sandboxResetTask } from "@/worker/tasks/sandbox-reset";
+import { sandboxResumeTask } from "@/worker/tasks/sandbox-resume";
 
 export const taskList = {
   "agent-run-resume": agentRunResumeTask,
   "agent-team-run": agentTeamRunTask,
   "document-parse": documentParseTask,
+  "sandbox-create": sandboxCreateTask,
+  "sandbox-execute": sandboxExecuteTask,
+  "sandbox-resume": sandboxResumeTask,
+  "sandbox-reset": sandboxResetTask,
+  "sandbox-cleanup": sandboxCleanupTask,
+  "sandbox-artifact-cleanup": sandboxCleanupTask,
+  "sandbox-health-check": sandboxCleanupTask,
 } satisfies TaskList;
 
 export const supportedWorkerTaskNames = Object.freeze(Object.keys(taskList));
