1355d05152335116b34cefedba2b4818e204705e
Mtzallqmy <mtzallqmy@gmail.com>
2026-08-07 03:36:05 +0300

    feat(worker): register Telegram update processor

--- src/worker/task-list.ts
+++ src/worker/task-list.ts
@@ -10,6 +10,7 @@ import { sandboxCreateTask } from "@/worker/tasks/sandbox-create";
 import { sandboxExecuteTask } from "@/worker/tasks/sandbox-execute";
 import { sandboxResetTask } from "@/worker/tasks/sandbox-reset";
 import { sandboxResumeTask } from "@/worker/tasks/sandbox-resume";
+import { telegramUpdateProcessTask } from "@/worker/tasks/telegram-update-process";
 
 export const taskList = {
   "agent-run-resume": agentRunResumeTask,
@@ -18,6 +19,7 @@ export const taskList = {
   "browser-task-resume": browserTaskResumeTask,
   "document-parse": documentParseTask,
   "notification-dispatch": notificationDispatchTask,
+  "telegram-update-process": telegramUpdateProcessTask,
   "sandbox-create": sandboxCreateTask,
   "sandbox-execute": sandboxExecuteTask,
   "sandbox-resume": sandboxResumeTask,
